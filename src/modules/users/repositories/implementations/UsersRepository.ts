eca663a828dcfc5e89b6b007244d1c09f2a4bb2d
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2021-06-22 14:01:53 -0300

    challenge solved

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,42 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const findUser = this.users.find((user) => user.id === id);
+
+    return findUser;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const findUser = this.users.find((user) => user.email === email);
+
+    return findUser;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const userIndex = this.users.findIndex((user) => user === receivedUser);
+
+    Object.assign(this.users[userIndex], {
+      admin: true,
+    });
+
+    return this.users[userIndex];
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
